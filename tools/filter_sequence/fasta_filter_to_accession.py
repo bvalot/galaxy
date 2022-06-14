8e2f5bda777bb0c83b49bc719615441b1aabeb6e
Benoit Valot <benoit.valot@univ-fcomte.fr>
2022-06-14 09:37:15 +0200

    Syntax correction for flake8 check

--- tools/filter_sequence/fasta_filter_to_accession.py
+++ tools/filter_sequence/fasta_filter_to_accession.py
@@ -8,25 +8,36 @@ import argparse
 from Bio import SeqIO
 
 desc = "Filter a fasta file based on ."
-command = argparse.ArgumentParser(prog='fasta_filter_to_accession', \
-    description=desc, usage='%(prog)s [options] fasta accessions')
-command.add_argument('-o','--output', default=sys.stdout, \
-    type=argparse.FileType("w"), nargs='?', \
-    help='Output result to, default:stdout')
-command.add_argument('fasta', type=argparse.FileType("r"), \
-    help='Fasta file to filter')
-command.add_argument('access', metavar='accessions', \
-    type=str, nargs='+', \
-    help='List of accession to extract')
+command = argparse.ArgumentParser(
+    prog="fasta_filter_to_accession",
+    description=desc,
+    usage="%(prog)s [options] fasta accessions",
+)
+command.add_argument(
+    "-o",
+    "--output",
+    default=sys.stdout,
+    type=argparse.FileType("w"),
+    nargs="?",
+    help="Output result to, default:stdout",
+)
+command.add_argument("fasta", type=argparse.FileType("r"), help="Fasta file to filter")
+command.add_argument(
+    "access",
+    metavar="accessions",
+    type=str,
+    nargs="+",
+    help="List of accession to extract",
+)
 
-if __name__=='__main__':
-    """Performed job on execution script""" 
+if __name__ == "__main__":
+    """Performed job on execution script"""
     args = command.parse_args()
     output = args.output
     access = set(args.access)
-    for seq in SeqIO.parse(args.fasta, 'fasta'):
+    for seq in SeqIO.parse(args.fasta, "fasta"):
         if seq.id in access:
-            SeqIO.write(seq, output, 'fasta')
+            SeqIO.write(seq, output, "fasta")
             access.remove(seq.id)
     if len(access) > 0:
-        sys.stderr.write("Accessions not found : " + str(' - ').join(access) + "\n")
+        sys.stderr.write("Accessions not found : " + str(" - ").join(access) + "\n")
